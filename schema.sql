6cda2a94ef4ab1d81dd2088dcbd47722e5f376c3
Николай <arcmag-work@mail.ru>
2019-12-29 16:52:37 +0700

    Обновляет схему БД

--- schema.sql
+++ schema.sql
@@ -4,10 +4,10 @@ USE `task-force`;
 
 CREATE TABLE `user` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `login` varchar(255),
-  `email` varchar(255),
-  `password` varchar(255),
-  `date_registration` timestamp
+  `login` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `date_registration` timestamp DEFAULT now()
 );
 
 CREATE TABLE `user_data` (
@@ -28,8 +28,8 @@ CREATE TABLE `user_data` (
 
 CREATE TABLE `user_settings` (
   `user_id` int,
-  `is_hidden_contacts` bool,
-  `is_hidden_profile` bool
+  `is_hidden_contacts` boolean,
+  `is_hidden_profile` boolean
 );
 
 CREATE TABLE `user_photo` (
@@ -37,15 +37,9 @@ CREATE TABLE `user_photo` (
   `photo` varchar(255)
 );
 
-CREATE TABLE `specialization` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `title` varchar(255),
-  `code` varchar(255)
-);
-
 CREATE TABLE `user_specialization` (
   `user_id` int,
-  `specialization_id` int
+  `category_id` int
 );
 
 CREATE TABLE `task` (
@@ -54,11 +48,12 @@ CREATE TABLE `task` (
   `title` varchar(255),
   `description` text,
   `category_id` int,
-  `price` int,
+  `price` int NOT NULL,
   `date_start` timestamp,
   `date_end` timestamp,
   `executor_id` int,
-  `status` varchar(255)
+  `is_telework` boolean DEFAULT FALSE,
+  `status` boolean DEFAULT TRUE
 );
 
 CREATE TABLE `task_file` (
@@ -68,13 +63,15 @@ CREATE TABLE `task_file` (
 
 CREATE TABLE `task_respond` (
   `task_id` int,
-  `user_id` int
+  `user_id` int,
+  `text` text,
+  `public_date` timestamp
 );
 
 CREATE TABLE `category` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `title` varchar(255),
-  `code` varchar(255)
+  `title` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL
 );
 
 CREATE TABLE `chat` (
@@ -86,47 +83,56 @@ CREATE TABLE `chat` (
 CREATE TABLE `message` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `chat_id` int,
-  `author_id` int,
+  `author_id` int NOT NULL,
   `public_date` timestamp,
   `text` text
 );
 
 CREATE TABLE `review` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
+  `task_id` int,
   `author_id` int,
   `executor_id` int,
-  `text` text,
-  `rating` int
+  `text` text
 );
 
-ALTER TABLE `user_data` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+CREATE TABLE `city` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `lat` float,
+  `long` float
+);
 
-ALTER TABLE `user_settings` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
-
-ALTER TABLE `user_photo` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
-
-ALTER TABLE `user_specialization` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
-
-ALTER TABLE `user_specialization` ADD FOREIGN KEY (`specialization_id`) REFERENCES `specialization` (`id`);
-
-ALTER TABLE `task` ADD FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
-
-ALTER TABLE `task` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE;
-
-ALTER TABLE `task` ADD FOREIGN KEY (`executor_id`) REFERENCES `user` (`id`);
-
-ALTER TABLE `task_file` ADD FOREIGN KEY (`task_id`) REFERENCES `task` (`id`) ON DELETE CASCADE;
-
-ALTER TABLE `task_respond` ADD FOREIGN KEY (`task_id`) REFERENCES `task` (`id`) ON DELETE CASCADE;
-
-ALTER TABLE `task_respond` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
-
-ALTER TABLE `chat` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
-
-ALTER TABLE `chat` ADD FOREIGN KEY (`task_id`) REFERENCES `task` (`id`);
-
-ALTER TABLE `message` ADD FOREIGN KEY (`chat_id`) REFERENCES `chat` (`id`) ON DELETE CASCADE;
-
-ALTER TABLE `review` ADD FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
-
-ALTER TABLE `review` ADD FOREIGN KEY (`executor_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+-- ALTER TABLE `user_data` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+--
+-- ALTER TABLE `user_settings` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+--
+-- ALTER TABLE `user_photo` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+--
+-- ALTER TABLE `user_specialization` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+--
+-- ALTER TABLE `user_specialization` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+--
+-- ALTER TABLE `task` ADD FOREIGN KEY (`author_id`) REFERENCES `user` (`id`);
+--
+-- ALTER TABLE `task` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+--
+-- ALTER TABLE `task` ADD FOREIGN KEY (`executor_id`) REFERENCES `user` (`id`);
+--
+-- ALTER TABLE `task_file` ADD FOREIGN KEY (`task_id`) REFERENCES `task` (`id`);
+--
+-- ALTER TABLE `task_respond` ADD FOREIGN KEY (`task_id`) REFERENCES `task` (`id`);
+--
+-- ALTER TABLE `task_respond` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+--
+-- ALTER TABLE `chat` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+--
+-- ALTER TABLE `chat` ADD FOREIGN KEY (`task_id`) REFERENCES `task` (`id`);
+--
+-- ALTER TABLE `message` ADD FOREIGN KEY (`chat_id`) REFERENCES `chat` (`id`);
+--
+-- ALTER TABLE `review` ADD FOREIGN KEY (`task_id`) REFERENCES `task` (`id`);
+--
+-- ALTER TABLE `review` ADD FOREIGN KEY (`author_id`) REFERENCES `user` (`id`);
+--
+-- ALTER TABLE `review` ADD FOREIGN KEY (`executor_id`) REFERENCES `user` (`id`);
